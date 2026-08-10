9fccab70614c85fc0a0a78a34bf6f6185222419d
Kiord <glenn.kerbiriou@gmail.com>
2026-08-10 21:04:18 +0200

    explicitely added the icon for linux build

--- wails_main.go
+++ wails_main.go
@@ -7,10 +7,14 @@ import (
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	linuxoptions "github.com/wailsapp/wails/v2/pkg/options/linux"
 )
 
 //go:embed all:web
 var assets embed.FS
+
+//go:embed assets/appicon.png
+var appIcon []byte
 
 func main() {
 	app := NewApp()
@@ -24,6 +28,10 @@ func main() {
 
 		AssetServer: &assetserver.Options{
 			Assets: assets,
+		},
+		Linux: &linuxoptions.Options{
+			Icon:        appIcon,
+			ProgramName: "spacebrowser",
 		},
 	})
 	if err != nil {
